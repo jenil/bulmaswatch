69c4f39a6f0309eda030970f5db5bef85d1ef7f3
Jenil Gogari <jenil27@hotmail.com>
2017-01-29 14:09:54 -0500

    minify and source🗺

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 gulp.task('serve', ['sass'], function() {
@@ -17,9 +19,15 @@ gulp.task('serve', ['sass'], function() {
 
 gulp.task('sass', function() {
   return gulp.src('*/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
+      outputStyle: 'compressed',
       includePaths: 'node_modules/bulma'
     }).on('error', sass.logError))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
 });
