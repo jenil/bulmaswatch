933d0fa84b19452e1c6f0dd910033b1137bd41f0
Jenil Gogari <jenil27@hotmail.com>
2017-01-30 02:08:03 -0500

    Add initial themes and  🏠 page

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,35 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
+var cp = require('child_process');
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('jekyll-build', function(done) {
+  browserSync.notify('Jekyll build');
+  cp.exec('bundle exec jekyll build', function(err, stdout, stderr) {
+    console.log(stdout);
+  }).on('exit', function(code) {
+        done(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+    });
+});
 
+gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
+  browserSync.reload();
+});
+
+
+gulp.task('serve', ['sass', 'jekyll-build'], function() {
   browserSync.init({
     server: {
-      baseDir: './',
-      directory: true
+      baseDir: '_site'
     }
   });
 
-  gulp.watch('*/*.scss', ['sass']);
-  gulp.watch('*/*.html').on('change', browserSync.reload);
+  gulp.watch(['*/*.scss', '!_site/**'], ['sass']).on('change', function(event) {
+    console.log('SCSS :File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+  gulp.watch(['**/*.{html,md}', '!_site/**'], ['jekyll-rebuild']).on('change', function(event) {
+    console.log('HTML :File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
 });
 
 gulp.task('sass', function() {
@@ -28,8 +45,9 @@ gulp.task('sass', function() {
       suffix: '.min'
     }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('_site/'))
+    .pipe(browserSync.stream())
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['serve']);
