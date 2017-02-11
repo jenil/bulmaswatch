8c93b5a6ed1334ec0da52e079bb1bd4b3b4efb40
Jenil Gogari <jenil27@hotmail.com>
2017-02-10 20:17:35 -0500

    ⚙ fix build process

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 var cp = require('child_process');
+var del = require('del');
+
+gulp.task('clean', function() {
+  return del(['*/*.css', '*/*.css.map']);
+});
 
 gulp.task('jekyll-build', function(done) {
   browserSync.notify('Jekyll build');
@@ -21,22 +26,36 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
 });
 
 
-gulp.task('serve', ['sass', 'jekyll-build'], function() {
+gulp.task('serve', ['sass:dev', 'jekyll-build'], function() {
   browserSync.init({
     server: {
       baseDir: '_site'
     }
   });
 
-  gulp.watch(['*/*.scss', '!_site/**'], ['sass']).on('change', function(event) {
-    console.log('SCSS :File ' + event.path + ' was ' + event.type + ', running tasks...');
+  gulp.watch(['*/*.scss', '!_site/**'], ['sass:dev']).on('change', function(event) {
+    console.log('SCSS: File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
   gulp.watch(['**/*.{html,md}', '!_site/**'], ['jekyll-rebuild']).on('change', function(event) {
-    console.log('HTML :File ' + event.path + ' was ' + event.type + ', running tasks...');
+    console.log('HTML: File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
 });
 
-gulp.task('sass', function() {
+gulp.task('sass:dev', ['clean'], function() {
+  return gulp.src('*/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: 'node_modules/bulma'
+    }).on('error', sass.logError))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('_site/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', ['clean'], function() {
   return gulp.src('*/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
@@ -47,10 +66,8 @@ gulp.task('sass', function() {
     }))
     .pipe(autoprefixer())
     .pipe(cssnano())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('_site/'))
-    .pipe(browserSync.stream())
-    .pipe(gulp.dest('./'));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('default', ['serve']);
