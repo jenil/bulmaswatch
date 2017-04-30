9d6e85863809d881adc27188450f08a42f3770fb
Jenil Gogari <jenil27@gmail.com>
2017-04-29 20:06:11 -0400

    restructure app for new build process

--- gulpfile.js
+++ gulpfile.js
@@ -18,102 +18,102 @@ const CDN = 'https://cdn.rawgit.com/jenil/bulmaswatch/gh-pages';
 var changedTheme = '';
 
 gulp.task('clean', function() {
-  return del(['*/*.css', '*/*.css.map']);
+    return del(['*/*.css', '*/*.css.map']);
 });
 
 gulp.task('jekyll-build', function(done) {
-  browserSync.notify('Jekyll build');
-  cp.exec('bundle exec jekyll build --config _config.yml,_config.local.yml', function(err, stdout, stderr) {
-    console.log(stdout);
-  }).on('exit', function(code) {
-    done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
-  });
+    browserSync.notify('Jekyll build');
+    cp.exec('bundle exec jekyll build --config _config.yml,_config.local.yml', function(err, stdout, stderr) {
+        console.log(stdout);
+    }).on('exit', function(code) {
+        done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+    });
 });
 
 gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
-  browserSync.reload();
+    browserSync.reload();
 });
 
 
 gulp.task('serve', ['sass:dev', 'jekyll-build'], function() {
-  browserSync.init({
-    server: {
-      baseDir: '_site'
-    },
-    ghostMode: {
-      clicks: false,
-      forms: false
-    }
-  });
+    browserSync.init({
+        server: {
+            baseDir: '_site'
+        },
+        ghostMode: {
+            clicks: false,
+            forms: false
+        }
+    });
 
-  gulp.watch(['*/*.scss', '!_site/**'], ['sass:dev']).on('change', function(event) {
-    console.log('SCSS: File ' + event.path + ' was ' + event.type + ', running tasks...');
-    changedTheme = event.path.split('/')[event.path.split('/').length - 2]
-  });
-  gulp.watch(['**/*.{html,md}', '!_site/**'], ['jekyll-rebuild']).on('change', function(event) {
-    console.log('HTML: File ' + event.path + ' was ' + event.type + ', running tasks...');
-  });
+    gulp.watch(['*/*.scss', '!_site/**'], ['sass:dev']).on('change', function(event) {
+        console.log('SCSS: File ' + event.path + ' was ' + event.type + ', running tasks...');
+        changedTheme = event.path.split('/')[event.path.split('/').length - 2]
+    });
+    gulp.watch(['**/*.{html,md}', '!_site/**'], ['jekyll-rebuild']).on('change', function(event) {
+        console.log('HTML: File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
 
 gulp.task('sass:dev', function() {
-  console.log('Building', changedTheme);
-  return gulp.src(changedTheme ? `${changedTheme}/*.scss` : '*/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      includePaths: 'node_modules/bulma'
-    }).on('error', sass.logError))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(changedTheme ? `_site/${changedTheme}` : '_site/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-    .pipe(gulp.dest(changedTheme ? `_site/${changedTheme}` : '.'));
+    console.log('Building', changedTheme);
+    return gulp.src(changedTheme ? `${changedTheme}/*.scss` : '*/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: 'node_modules/bulma'
+        }).on('error', sass.logError))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(changedTheme ? `_site/${changedTheme}` : '_site/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+        .pipe(gulp.dest(changedTheme ? `_site/${changedTheme}` : '.'));
 });
 
 gulp.task('sass', ['clean'], function() {
-  return gulp.src('*/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      includePaths: 'node_modules/bulma'
-    }).on('error', sass.logError))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(autoprefixer())
-    .pipe(cssnano())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('.'));
+    return gulp.src('*/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: 'node_modules/bulma'
+        }).on('error', sass.logError))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('api', function() {
-  var API = {
-    version: VERSION,
-    themes: []
-  };
+    var API = {
+        version: VERSION,
+        themes: []
+    };
 
-  del(['api/*']);
+    del(['api/*']);
 
-  return gulp.src('./_themes/*.md')
-    .pipe(frontMatter({
-      property: 'meta'
-    }))
-    .pipe(data(function(file) {
-      delete file.meta.order;
-      file.meta.preview = HOME + '/' + file.meta.name.toLowerCase() + '/';
-      file.meta.thumb = HOME + '/thumb/?' + file.meta.name.toLowerCase();
-      file.meta.css = CDN + '/' + file.meta.name.toLowerCase() + '/bulmaswatch.min.css';
-      file.meta.scss = CDN + '/' + file.meta.name.toLowerCase() + '/bulmaswatch.scss';
-      file.meta.scssVariables = CDN + '/' + file.meta.name.toLowerCase() + '/_variables.scss';
-    }))
-    .pipe(pluck('meta', 'themes.json'))
-    .pipe(data(function(file) {
-      API.themes = file.meta;
-      file.contents = new Buffer(JSON.stringify(API));
-    }))
-    .pipe(gulp.dest('api'));
+    return gulp.src('./_themes/*.md')
+        .pipe(frontMatter({
+            property: 'meta'
+        }))
+        .pipe(data(function(file) {
+            delete file.meta.order;
+            file.meta.preview = HOME + '/' + file.meta.name.toLowerCase() + '/';
+            file.meta.thumb = HOME + '/thumb/?' + file.meta.name.toLowerCase();
+            file.meta.css = CDN + '/' + file.meta.name.toLowerCase() + '/bulmaswatch.min.css';
+            file.meta.scss = CDN + '/' + file.meta.name.toLowerCase() + '/bulmaswatch.scss';
+            file.meta.scssVariables = CDN + '/' + file.meta.name.toLowerCase() + '/_variables.scss';
+        }))
+        .pipe(pluck('meta', 'themes.json'))
+        .pipe(data(function(file) {
+            API.themes = file.meta;
+            file.contents = new Buffer(JSON.stringify(API));
+        }))
+        .pipe(gulp.dest('api'));
 });
 
 gulp.task('default', ['serve']);
